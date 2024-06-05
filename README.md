2eb6d58fbd2991f703c8e40946a14aa904332804
Juhyi <dlwngml201@naver.com>
2024-06-05 11:48:16 +0900

    웹페이지 에니메이션 튜토리얼 완료

--- README.md
+++ README.md
@@ -190,9 +190,18 @@ IoT 개발자과정 ASP.NET 리포지토리
 
         https://github.com/Juhyi/Basic-aspnet-2024/assets/158007405/5b29e988-0e2e-4200-a5f3-28b939a27b11
 
-        - Codehel 유튜버 로그인 웹페이지 튜토리얼 따라하기
+        - Codehal 유튜버 로그인 웹페이지 튜토리얼 따라하기
 
 
         <img src="https://raw.githubusercontent.com/Juhyi/Basic-aspnet-2024/main/images/an001.png" width="730" alt="codehal 로그인창 따라하기">
 
-        - codehel 슬라이더 에니멩션 웹페이지 튜토리얼 따라힉
+        - codehel 슬라이더 에니메이션 웹페이지 튜토리얼 따라하기
+
+## 6일차
+- HTML + CSS + js(jQuery) 응용
+    - 웹페이지 클로닝
+        - codehal 슬라이더 에니메이션 웹페이지 튜토리얼 따라하기 (완료)
+
+    
+
+    - 개인 웹페이지 클로닝
---
+++ day05/js/sub.js
@@ -0,0 +1,29 @@
+/* 회전버튼 동작 */
+const rotateBtn = document.querySelector('.rotate-btn');
+const slides = document.querySelectorAll('.bg-slide');
+const totalSlides = slides.length; // 5개
+let index = 0;
+
+// $('.rotate-btn').on('click', funtion()); 와 동일
+rotateBtn.addEventListener('click', function(){
+    rotateBtn.classList.add('active');
+    setTimeout(()=> {
+        rotateBtn.classList.remove('active');
+    },2100);
+
+    slides.forEach(slide => {
+        if (slide.classList.contains('active')){
+            slide.classList.add('after-active');
+        }else{
+            slide.classList.remove('after-active');
+        }
+    })
+
+    slides[index].classList.remove('active'); // 현재 슬라이드는 active 제거
+    index += 1;
+
+    if (index == totalSlides) index = 0;
+
+    slides[index].classList.add('active');
+});
+
